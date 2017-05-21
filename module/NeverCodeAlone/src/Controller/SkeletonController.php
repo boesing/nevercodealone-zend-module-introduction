ee58ab9f49d8cc619a6f5cf57b439ec22ce36249
Max Bösing <max.boesing@check24.de>
2017-05-21 16:50:22 +0200

    Initialize `zend-module-introduction` project

---
+++ module/NeverCodeAlone/src/Controller/SkeletonController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/NeverCodeAlone for the canonical source repository
+ * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace NeverCodeAlone\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class SkeletonController extends AbstractActionController
+{
+
+    /**
+     * @return array
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
---
+++ module/NeverCodeAlone/src/Module.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/NeverCodeAlone for the canonical source repository
+ * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace NeverCodeAlone;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+
+class Module /* implements ConfigProviderInterface, DependencyIndicatorInterface */
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfig()
+    {
+        $provider = new ConfigProvider();
+
+        return [
+            'service_manager' => $provider->getDependencyConfig(),
+            'router' => $provider->getRouterConfig(),
+            'controllers' => $provider->getControllerConfig(),
+            'view_manager' => $provider->getViewManagerConfig(),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getModuleDependencies()
+    {
+        return [
+            'Zend\Router',
+        ];
+    }
+}
