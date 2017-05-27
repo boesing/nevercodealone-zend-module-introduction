1b86d0745d21bf1da53edb0be1e4d1bb63d3177c
Max Bösing <max.boesing@check24.de>
2017-05-27 10:13:39 +0200

    Remove header

--- module/NeverCodeAlone/src/Controller/SkeletonController.php
+++ module/NeverCodeAlone/src/Controller/SkeletonController.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @link      http://github.com/zendframework/NeverCodeAlone for the canonical source repository
- * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace NeverCodeAlone\Controller;
 
